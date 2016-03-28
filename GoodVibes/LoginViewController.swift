28eaf3cf7d99a57b96bbcdee4ba3e69317ed3f7f
gquerbes <gquerbes@gmail.com>
2016-03-27 21:55:25 -0400

    BigUpdate
    Added account creation and login functions.

---
+++ GoodVibes/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  GoodVibes
+//
+//  Created by Gabriel Querbes on 3/27/16.
+//  Copyright © 2016 The Modern Tech. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnLogin(sender: AnyObject) {
+        let alert = UIAlertController(title: "", message: "", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        PFUser.logInWithUsernameInBackground(txtUsername.text!, password: txtPassword.text!){
+            (user: PFUser?, error:NSError?) -> Void in
+            if user != nil{
+               print("success!")
+                alert.title = "Success!"
+                alert.message = "You are now logged in."
+            }
+            else{
+                print("Access Denied")
+                alert.title = "Error"
+                alert.message = "Access Denied"
+            }
+        }
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+            
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- GoodVibes/ViewController.swift
+++ GoodVibes/ViewController.swift
@@ -7,13 +7,49 @@
 //
 
 import UIKit
+import Parse
+
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var txtEmailAddress: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtUsername: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let testObject = PFObject(className: "TestObject")
+        testObject["foo"] = "bar"
+        testObject.saveInBackgroundWithBlock { (success, error) -> Void in
+            print("Object has been saved.")
+        }
     }
+    
+    @IBAction func btnCreateAccount(sender: AnyObject) {
+        var user = PFUser()
+        user.username = txtUsername.text
+        user.password = txtPassword.text
+        user.email = txtEmailAddress.text
+        
+        user.signUpInBackgroundWithBlock{
+            (succeeded: Bool, error: NSError?) -> Void in
+            if let error = error {
+                let errorString = error.userInfo["error"] as? NSString
+                //show error string somewhere and let user retry
+                print (errorString)
+            }
+            else{
+                //you have logged in!
+                print( "Welcome New User!")
+            }
+            
+        }
+
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()

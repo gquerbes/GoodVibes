b822654b1644f12668a205dedf1e21c55b82e8db
gquerbes <gquerbes@gmail.com>
2016-04-02 17:54:23 -0400

    added ability to update account information

--- GoodVibes/AccountUpdateViewController.swift
+++ GoodVibes/AccountUpdateViewController.swift
@@ -7,15 +7,70 @@
 //
 
 import UIKit
+import Parse
 
 class AccountUpdateViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtMobileNumber: UITextField!
+    @IBOutlet weak var txtStreetAddress: UITextField!
+    
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    
+    var user: PFUser?
+    
+    @IBAction func btnUpdateAccount(sender: AnyObject) {
+        updateAccount()
     }
 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        // Do any additional setup after loading the view.
+    }
+    
+    func configureView(){
+    
+        PFUser.currentUser()!.fetchInBackgroundWithBlock({ (currentUser: PFObject?, error: NSError?) -> Void in
+            // Update your data     
+            if let user = currentUser as? PFUser {
+                self.user = user
+                self.txtUsername.text = user.username
+                self.txtEmail.text = user.email
+                self.txtPassword.text = "****Chage To Update****" //add functionality
+                
+                //get custom attributes from user document and pass to text boxes
+                self.txtMobileNumber.text = user.objectForKey("mobile_number") as? String
+                let address = user.objectForKey("address")
+                self.txtStreetAddress.text = address!.objectForKey("street_address") as? String
+                self.txtState.text = address!.objectForKey("state") as? String
+                self.txtCity.text = address!.objectForKey("city") as? String
+                self.txtZip.text = address!.objectForKey("zip") as? String
+                
+                
+            
+            }
+        })
+    }
+
+    
+    func updateAccount(){
+        //self.user?.username = txtUsername.text
+        self.user?["mobile_number"] = txtMobileNumber.text
+        self.user?["address.street_address"] = txtStreetAddress.text
+        self.user?["address.city"] = txtCity.text
+        self.user?["address.state"] = txtState.text
+        self.user?["address.zip"] = txtZip.text
+        
+        
+        user?.saveInBackground()
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
